c335042fcca4247bdb685d1cf60d452fd1717c33
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2022-07-17 00:58:22 +0300

    feat: add slider

--- travel/index.js
+++ travel/index.js
@@ -35,7 +35,118 @@ document.addEventListener('click', function(e){
     }}, true);
 
 
-console.log(`Travel#2.
+// Slider
+
+let caroosel = document.querySelector('.caroosel');
+let arrowLeft = document.querySelector('.dest_navigation_left');
+let arrowRight = document.querySelector('.dest_navigation_right');
+let slide0 = document.querySelector('.item0');
+let slide1 = document.querySelector('.item1');
+let slide2 = document.querySelector('.item2');
+let slide3 = document.querySelector('.item3');
+let slide4 = document.querySelector('.item4');
+let slider = document.querySelectorAll('.slider');
+
+let moveLeft = () => {
+  caroosel.classList.add('to-left');
+  slide1.removeEventListener('click', moveLeft);
+  slide3.removeEventListener('click', moveRight);
+  arrowLeft.removeEventListener('click', moveLeft);
+  arrowRight.removeEventListener('click', moveRight);
+
+  for (let i = 0; i < slider.length; i++) {
+      if (slider[i] == document.querySelector('.slider_active')) {
+        if (i == 0) {
+          slider[0].classList.remove('slider_active')
+          slider[2].classList.add('slider_active')
+          return;
+        } else if (i == 1) {
+          slider[1].classList.remove('slider_active')
+          slider[0].classList.add('slider_active')
+          return;
+        } else if (i == 2) {
+          slider[2].classList.remove('slider_active')
+          slider[1].classList.add('slider_active')
+          return;
+        }
+      }
+    }
+  };
+
+let moveRight = () => {
+  caroosel.classList.add('to-right');
+  slide1.removeEventListener('click', moveRight);
+  slide3.removeEventListener('click', moveLeft);
+  arrowLeft.removeEventListener('click', moveRight);
+  arrowRight.removeEventListener('click', moveLeft);
+
+  for (let i = 0; i < slider.length; i++) {
+      if (slider[i] == document.querySelector('.slider_active')) {
+        if (i == 0) {
+          slider[0].classList.remove('slider_active')
+          slider[1].classList.add('slider_active')
+          return
+        } else if (i == 1) {
+          slider[1].classList.remove('slider_active')
+          slider[2].classList.add('slider_active')
+          return
+        } else if (i == 2) {
+          slider[2].classList.remove('slider_active')
+          slider[0].classList.add('slider_active')
+          return
+        }
+      }
+    }
+  };
+
+arrowLeft.addEventListener('click', moveLeft);
+arrowRight.addEventListener('click', moveRight);
+slide1.addEventListener('click', moveLeft);
+slide3.addEventListener('click', moveRight);
+
+
+
+caroosel.addEventListener('animationend', (animation) => {
+
+  let active = slide2.innerHTML;
+
+  if (animation.animationName === 'move-left') {
+    caroosel.classList.remove('to-left');
+    slide2.innerHTML = slide1.innerHTML;
+    slide1.innerHTML = slide0.innerHTML;
+    slide3.innerHTML = active;
+    slide0.innerHTML = active;
+    slide4.innerHTML = slide1.innerHTML;
+  } else {
+    caroosel.classList.remove('to-right');
+    slide2.innerHTML = slide3.innerHTML;
+		slide3.innerHTML = slide4.innerHTML;
+		slide1.innerHTML = active;
+		slide4.innerHTML = active;
+		slide0.innerHTML = slide3.innerHTML;
+  }
+
+  arrowLeft.addEventListener('click', moveLeft);
+  arrowRight.addEventListener('click', moveRight);
+  slide1.addEventListener('click', moveLeft);
+  slide3.addEventListener('click', moveRight);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*console.log(`Travel#2.
 \n1.Вёрстка соответствует макету. Ширина экрана 390px +48
 -блок <header> +6
 -секция preview +9
@@ -54,4 +165,4 @@ console.log(`Travel#2.
 -ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +4 (все кроме Account, она пока что просто закрывает меню)
 -при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, также скрытие меню происходит если сделать клик вне данного окна +4
 \nИтог: 85
-`);
+`);*/
