9432b43d8d398efc213db1bb4bb802cc94ddacb8
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2022-08-24 01:15:23 +0300

    feat: add tracker for azathoth, add change cards deck for azathoth

--- eldritch-horror-codejam/js/index.js
+++ eldritch-horror-codejam/js/index.js
@@ -3,41 +3,21 @@ import cardsDataBrown from './cardsDataBrown.js';
 import cardsDataBlue from './cardsDataBlue.js';
 import ancientsData from './ancientsData.js';
 
-
-const difficulties = [
-  {
-    id: 'easy',
-    name: 'Низкая'
-  },
-  {
-    id: 'normal',
-    name: 'Средняя'
-  },
-  {
-    id: 'hard',
-    name: 'Высокая'
-  },
-]
-
 const cardsBTN = document.querySelector('.cards_button');
 const levelMid = document.querySelector('.level3');
 const deckWrapper = document.querySelector('.deck_wrapper');
 const currentCard = document.querySelector('.current_card');
+const cardsDeck = document.querySelector('.cards_deck');
+const img = new Image();
 
-(function () {
-  levelMid.addEventListener('click', () => { 
-    cardsBTN.style.display = "block";
-    deckWrapper.style.display = "none";
-  })
-  cardsBTN.addEventListener('click', () => { 
-    cardsBTN.style.display = "none";
-    deckWrapper.style.display = "flex";
-    currentCard.style.visibility = "hidden";
-  })
-  deckWrapper.addEventListener('click', () => { 
-    currentCard.style.visibility = "visible";
-  })
-}());
+levelMid.addEventListener('click', () => { 
+  cardsBTN.style.display = "block";
+  deckWrapper.style.display = "none";
+})
+cardsBTN.addEventListener('click', () => { 
+  cardsBTN.style.display = "none";
+  deckWrapper.style.display = "flex";
+})
 
 let stageOne = [];
 let stageTwo = [];
@@ -67,7 +47,7 @@ for (let i=0; i<1; i++) {
     stageOne.push(cardsDataBlue[index]);
   } 
 }
-console.log(stageOne);
+console.log("stageOne = ", stageOne);
 for (let i=0; i<2; i++) {
   let index = getRandomNum(0, cardsDataGreen.length-1);
   if (!stageTwo.includes(index)) {
@@ -86,7 +66,7 @@ for (let i=0; i<1; i++) {
     stageTwo.push(cardsDataBlue[index]);
   } 
 }
-console.log(stageTwo);
+console.log("stageTwo = ", stageTwo);
 for (let i=0; i<2; i++) {
   let index = getRandomNum(0, cardsDataGreen.length-1);
   if (!stageThree.includes(index)) {
@@ -99,7 +79,7 @@ for (let i=0; i<4; i++) {
     stageThree.push(cardsDataBrown[index]);
   } 
 }
-console.log(stageThree);
+console.log("stageThree = ", stageThree);
 
 function sort(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -112,8 +92,95 @@ sort(stageOne);
 sort(stageTwo);
 sort(stageThree);
 
-arrayCards.push(stageThree);
-arrayCards.push(stageTwo);
 arrayCards.push(stageOne);
+arrayCards.push(stageTwo);
+arrayCards.push(stageThree);
 arrayCards = arrayCards.flat(Infinity);
-console.log(arrayCards);
+console.log("arrayCards = ", arrayCards);
+
+let stageOneDots = [];
+let stageTwoDots = [];
+let stageThreeDots = [];
+let arrayDots = [];
+
+function setDots() {
+  stageOneDots.push(ancientsData[0].firstStage);
+  document.querySelector('.green_st1').textContent = `${ancientsData[0].firstStage.greenCards}`;
+  document.querySelector('.brown_st1').textContent = `${ancientsData[0].firstStage.brownCards}`;
+  document.querySelector('.blue_st1').textContent = `${ancientsData[0].firstStage.blueCards}`;
+  console.log("stageOneDots =", stageOneDots);
+  stageTwoDots.push(ancientsData[0].secondStage);
+  document.querySelector('.green_st2').textContent = `${ancientsData[0].secondStage.greenCards}`;
+  document.querySelector('.brown_st2').textContent = `${ancientsData[0].secondStage.brownCards}`;
+  document.querySelector('.blue_st2').textContent = `${ancientsData[0].secondStage.blueCards}`;
+  console.log("stageTwoDots =", stageTwoDots);
+  stageThreeDots.push(ancientsData[0].thirdStage);
+  document.querySelector('.green_st3').textContent = `${ancientsData[0].thirdStage.greenCards}`;
+  document.querySelector('.brown_st3').textContent = `${ancientsData[0].thirdStage.brownCards}`;
+  document.querySelector('.blue_st3').textContent = `${ancientsData[0].thirdStage.blueCards}`;
+  console.log("stageThreeDots =", stageThreeDots);
+}
+setDots();
+
+function setCard() {
+    for (let i = 0; i < 1; i++) {
+        if (arrayCards[i].color === "green") {
+          img.src = `assets/img/MythicCards/green/${arrayCards[i].id}.png`;
+          img.onload = () => {
+            currentCard.style.backgroundImage = `url(${img.src})`;
+          } 
+          if (stageOneDots[0].greenCards !== 0) {
+            document.querySelector('.green_st1').textContent = `${stageOneDots[0].greenCards - 1}`;
+            stageOneDots[0].greenCards = stageOneDots[0].greenCards - 1;
+          } else if (stageOneDots[0].greenCards === 0 && stageTwoDots[0].greenCards !== 0) {
+            document.querySelector('.green_st2').textContent = `${stageTwoDots[0].greenCards - 1}`;
+            stageTwoDots[0].greenCards = stageTwoDots[0].greenCards - 1;
+          } else if (stageTwoDots[0].greenCards === 0 && stageThreeDots[0].greenCards !== 0) {
+            document.querySelector('.green_st3').textContent = `${stageThreeDots[0].greenCards - 1}`;
+            stageThreeDots[0].greenCards = stageThreeDots[0].greenCards - 1;
+          }
+        } else if (arrayCards[i].color === "blue") {
+          img.src = `assets/img/MythicCards/blue/${arrayCards[i].id}.png`;
+          img.onload = () => {
+            currentCard.style.backgroundImage = `url(${img.src})`;
+          }
+          if (stageOneDots[0].blueCards !== 0) {
+            document.querySelector('.blue_st1').textContent = `${stageOneDots[0].blueCards - 1}`;
+            stageOneDots[0].blueCards = stageOneDots[0].blueCards - 1;
+          } else if (stageOneDots[0].blueCards === 0 && stageTwoDots[0].blueCards !== 0) {
+            document.querySelector('.blue_st2').textContent = `${stageTwoDots[0].blueCards - 1}`;
+            stageTwoDots[0].blueCards = stageTwoDots[0].blueCards - 1;
+          } else if (stageTwoDots[0].blueCards === 0 && stageThreeDots[0].blueCards !== 0) {
+            document.querySelector('.blue_st3').textContent = `${stageThreeDots[0].blueCards - 1}`;
+            stageThreeDots[0].blueCards = stageThreeDots[0].blueCards - 1;
+          }
+        } else if (arrayCards[i].color === "brown") {
+          img.src = `assets/img/MythicCards/brown/${arrayCards[i].id}.png`;
+          img.onload = () => {
+            currentCard.style.backgroundImage = `url(${img.src})`;
+          }
+          if (stageOneDots[0].brownCards !== 0) {
+            document.querySelector('.brown_st1').textContent = `${stageOneDots[0].brownCards - 1}`;
+            stageOneDots[0].brownCards = stageOneDots[0].brownCards - 1;
+          } else if (stageOneDots[0].brownCards === 0 && stageTwoDots[0].brownCards !== 0) {
+            document.querySelector('.brown_st2').textContent = `${stageTwoDots[0].brownCards - 1}`;
+            stageTwoDots[0].brownCards = stageTwoDots[0].brownCards - 1;
+          } else if (stageTwoDots[0].brownCards === 0 && stageThreeDots[0].brownCards !== 0) {
+            document.querySelector('.brown_st3').textContent = `${stageThreeDots[0].brownCards - 1}`;
+            stageThreeDots[0].brownCards = stageThreeDots[0].brownCards - 1;
+          }
+        }
+        arrayCards.shift();
+        if (arrayCards.length === 0) {
+          cardsDeck.style.visibility = "hidden";
+        }
+  }
+}
+
+deckWrapper.addEventListener('click', () => { 
+  if (arrayCards.length !== 0) {
+  setCard();
+  }
+})
+
+
