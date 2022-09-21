b7ed3f7fd9eadd701c54204b949917ab54a30450
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2022-09-21 17:25:53 +0300

    fix: edit email form submit style, add slider btns hover

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -6,6 +6,9 @@ const amountInput = document.querySelector('.another_amount_input');
 function correctForm(event) {
 	event.preventDefault();
   email.value = "";
+  email.style.border = '1px solid orange';
+  submitBTN.style.color = 'black';
+  submitBTN.style.border = '1px solid black';
 }
 emailForm .addEventListener('submit', correctForm);
 
@@ -13,14 +16,17 @@ function validate() {
   let pattern = /\S+@\S+/;
 
   if(email.value.match(pattern)) {
+    email.style.border = '1px solid green';
     submitBTN.style.color = 'green';
     submitBTN.style.border = '1px solid green';
   }
   else{
+    email.style.border = '1px solid red';
     submitBTN.style.color = 'red';
     submitBTN.style.border = '1px solid red';
   }
   if (email.value === "") {
+    email.style.border = '1px solid orange';
     submitBTN.style.color = 'black';
     submitBTN.style.border = '1px solid black';
   }
--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -5,6 +5,9 @@ const emailForm = document.querySelector('.enter_email');
 function correctForm(event) {
 	event.preventDefault();
   email.value = "";
+  email.style.border = '1px solid orange';
+  submitBTN.style.color = 'black';
+  submitBTN.style.border = '1px solid black';
 }
 emailForm .addEventListener('submit', correctForm);
 
@@ -12,14 +15,17 @@ function validate() {
   let pattern = /\S+@\S+/;
 
   if(email.value.match(pattern)) {
+    email.style.border = '1px solid green';
     submitBTN.style.color = 'green';
     submitBTN.style.border = '1px solid green';
   }
   else{
+    email.style.border = '1px solid red';
     submitBTN.style.color = 'red';
     submitBTN.style.border = '1px solid red';
   }
   if (email.value === "") {
+    email.style.border = '1px solid orange';
     submitBTN.style.color = 'black';
     submitBTN.style.border = '1px solid black';
   }
