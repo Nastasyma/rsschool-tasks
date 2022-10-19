0481f98dbdcec6a8a5fbe925a47c883c915afaaa
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2022-10-19 22:56:47 +0300

    fix: edit field 2-2, victory popup

--- gem-puzzle/js/index.js
+++ gem-puzzle/js/index.js
@@ -13,7 +13,7 @@ function createHeader() {
   const header_container = document.createElement('div');
   header_container.classList.add('header_container');
   const btn1 = document.createElement('button');
-  btn1.classList.add('start_btn');
+  btn1.classList.add('restart_btn');
   btn1.textContent = 'Restart game';
   const btn2 = document.createElement('button');
   btn2.classList.add('pause_btn');
@@ -89,6 +89,7 @@ function createMain() {
   const span5 = document.createElement('span');
   span5.textContent = 'Frame size:';
   const span6 = document.createElement('span');
+  span6.classList.add('change_size');
   span6.textContent = '4x4';
   current_field_size.appendChild(span5);
   current_field_size.appendChild(span6);
@@ -118,6 +119,10 @@ function createFooter() {
   const sizes_wrapper = document.createElement('div');
   sizes_wrapper.classList.add('sizes_wrapper');
   footer_container.appendChild(sizes_wrapper);
+  const btn0 = document.createElement('button');
+  btn0.classList.add('size_btn');
+  btn0.classList.add('field_2_2');
+  btn0.textContent = '2x2';
   const btn1 = document.createElement('button');
   btn1.classList.add('size_btn');
   btn1.classList.add('field_3_3');
@@ -143,6 +148,7 @@ function createFooter() {
   btn6.classList.add('size_btn');
   btn6.classList.add('field_8_8');
   btn6.textContent = '8x8';
+  sizes_wrapper.appendChild(btn0);
   sizes_wrapper.appendChild(btn1);
   sizes_wrapper.appendChild(btn2);
   sizes_wrapper.appendChild(btn3);
@@ -157,6 +163,8 @@ createFooter();
 const size_btns = document.querySelectorAll('.size_btn');
 const victory_popup = document.querySelector('.victory_popup');
 const hidden_wrapper = document.querySelector('.hidden_wrapper');
+const restart_btn = document.querySelector('.restart_btn');
+const change_size = document.querySelector('.change_size');
 
 size_btns.forEach(el => {
   el.addEventListener('click', (e) => {
@@ -201,9 +209,122 @@ let arrayNums8 = [];
 let arrayNums15 = [];
 
 const save_btn = document.querySelector('.save_btn');
+const btn_22 = document.querySelector('.field_2_2');
 const btn_33 = document.querySelector('.field_3_3');
 const btn_44 = document.querySelector('.field_4_4');
+const btn_55 = document.querySelector('.field_5_5');
+const btn_66 = document.querySelector('.field_6_6');
+const btn_77 = document.querySelector('.field_7_7');
+const btn_88 = document.querySelector('.field_8_8');
 
+function setGame3() {
+  canvas.width  = 300;
+  canvas.height = 300;
+  arrayNums3 = [0, 1, 2, 3];
+  sort(arrayNums3);
+  //console.log(arrayNums3);
+  const drawBox = function(x, y, value) {
+    context.fillStyle = "bisque";
+    context.fillRect(x, y, 150, 150);
+    context.shadowOffsetX = 2;
+    context.shadowOffsetY = 2;
+    context.shadowBlur = 2;
+    context.shadowColor = "black";
+    if (value === 0) {
+      context.fillStyle = "bisque";
+      context.shadowOffsetX = 0;
+      context.shadowOffsetY = 0;
+      context.shadowBlur = 0;
+      context.shadowColor = "none";
+    } else {
+      context.fillStyle = "midnightblue";
+    }
+    context.fillRect(x + 10, y + 10, 130, 130)
+    context.font = "38px Arial";
+    context.fillStyle = "bisque";
+    context.fillText(value, x + 65, y + 90);
+  }
+
+  const drawCell = function(position, value) {
+    if(position === 0) { drawBox(0, 0, value); }
+    else if (position === 1) { drawBox(150, 0, value); }
+    else if (position === 2) { drawBox(0, 150, value); }
+    else if (position === 3) { drawBox(150, 150, value); }
+  }
+
+  for (let i = 0; i<=8; i++) {
+    drawCell(i, arrayNums3[i])
+  }
+
+  const checkClientX = function(e) {
+    if (e < 150) { return 1; }
+    if (e < 300) { return 2; }
+  }
+
+  canvas.addEventListener("click", function(e) {
+    //console.log(e);
+    let check = checkClientX(e.offsetX);
+    if (e.offsetY < 150) {
+      switch(check) {
+        case 1: click = 0; break;
+        case 2: click = 1; break;
+      }
+    }
+    if (e.offsetY > 150 && e.offsetY < 300) {
+      switch(check) {
+        case 1: click = 2; break;
+        case 2: click = 3; break;
+      }
+    }
+    if (arrayNums3[click - 2] === 0) {
+      arrayNums3[click - 2] = arrayNums3[click];
+      arrayNums3[click] = 0;
+      counterMoves++;
+      counter_moves.textContent = counterMoves;
+    }
+    for (let i = 0; i<=3; i++) {
+      drawCell(i, arrayNums3[i])
+    }
+    if (arrayNums3[click + 2] === 0) {
+      arrayNums3[click + 2] = arrayNums3[click];
+      arrayNums3[click] = 0;
+      counterMoves++;
+      counter_moves.textContent = counterMoves;
+    }
+    for (let i = 0; i<=3; i++) {
+      drawCell(i, arrayNums3[i])
+    }
+    if (arrayNums3[click - 1] === 0 && click !== 2) {
+      arrayNums3[click - 1] = arrayNums3[click];
+      arrayNums3[click] = 0;
+      counterMoves++;
+      counter_moves.textContent = counterMoves;
+    }
+    for (let i = 0; i<=3; i++) {
+      drawCell(i, arrayNums3[i])
+    }
+    if (arrayNums3[click + 1] === 0 && click !== 1) {
+      arrayNums3[click + 1] = arrayNums3[click];
+      arrayNums3[click] = 0;
+      counterMoves++;
+      counter_moves.textContent = counterMoves;
+    }
+    for (let i = 0; i<=3; i++) {
+      drawCell(i, arrayNums3[i]);
+    }
+    //console.log(arrayNums3);
+    for (let i=0; i<arrayNums3.length; i++) {
+      if (arrayNums3[0] === 1
+        && arrayNums3[1] === 2
+        && arrayNums3[2] === 3
+        && arrayNums3[3] === 0) {
+        victory_popup.classList.add('popup_active');
+        victory_popup.textContent = `Hooray! You solved the puzzle in ${mins.textContent}:${sec.textContent} and ${counter_moves.textContent} moves!`;
+        hidden_wrapper.classList.add('hidden_wrapper_active');
+      }
+    }
+  });
+}
 function setGame8() {
   canvas.width  = 300;
   canvas.height = 300;
@@ -315,8 +436,18 @@ function setGame8() {
       drawCell(i, arrayNums8[i])
     }
     //console.log(arrayNums8);
-    if (arrayNums8 === [1, 2, 3, 4, 5, 6, 7, 8, 0]) {
-    alert ("Win!!");
+    if (arrayNums8[0] === 1
+      && arrayNums8[1] === 2
+      && arrayNums8[2] === 3
+      && arrayNums8[3] === 4
+      && arrayNums8[4] === 5
+      && arrayNums8[5] === 6
+      && arrayNums8[6] === 7
+      && arrayNums8[7] === 8
+      && arrayNums8[8] === 0) {
+      victory_popup.classList.add('popup_active');
+      victory_popup.textContent = `Hooray! You solved the puzzle in ${mins.textContent}:${sec.textContent} and ${counter_moves.textContent} moves!`;
+      hidden_wrapper.classList.add('hidden_wrapper_active');
     }
   });
 }
@@ -455,9 +586,26 @@ function setGame15() {
       drawCell(i, arrayNums15[i])
     }
       //console.log(arrayNums15);
-    if (arrayNums15 === [1, 2, 3, 4, 5, 6, 7, 8, 0]) {
-    alert ("Win!!");
-    }
+      if (arrayNums15[0] === 1
+        && arrayNums15[1] === 2
+        && arrayNums15[2] === 3
+        && arrayNums15[3] === 4
+        && arrayNums15[4] === 5
+        && arrayNums15[5] === 6
+        && arrayNums15[6] === 7
+        && arrayNums15[7] === 8
+        && arrayNums15[8] === 9
+        && arrayNums15[9] === 10
+        && arrayNums15[10] === 11
+        && arrayNums15[11] === 12
+        && arrayNums15[12] === 13
+        && arrayNums15[13] === 14
+        && arrayNums15[14] === 15
+        && arrayNums15[15] === 0) {
+        victory_popup.classList.add('popup_active');
+        victory_popup.textContent = `Hooray! You solved the puzzle in ${mins.textContent}:${sec.textContent} and ${counter_moves.textContent} moves!`;
+        hidden_wrapper.classList.add('hidden_wrapper_active');
+      }
   });
 
 /*let isMove = false;
@@ -489,116 +637,8 @@ function myUp(){
 canvas.onmousedown = myDown;
 canvas.onmouseup = myUp;*/
 }
-//setGame15();
-function setGame3() {
-  canvas.width  = 200;
-  canvas.height = 200;
-  arrayNums3 = [0, 1, 2, 3];
-  sort(arrayNums3);
-  //console.log(arrayNums3);
-  const drawBox = function(x, y, value) {
-    context.fillStyle = "bisque";
-    context.fillRect(x, y, 100, 100);
-    context.shadowOffsetX = 2;
-    context.shadowOffsetY = 2;
-    context.shadowBlur = 2;
-    context.shadowColor = "black";
-    if (value === 0) {
-      context.fillStyle = "bisque";
-      context.shadowOffsetX = 0;
-      context.shadowOffsetY = 0;
-      context.shadowBlur = 0;
-      context.shadowColor = "none";
-    } else {
-      context.fillStyle = "midnightblue";
-    }
-    context.fillRect(x + 5, y + 5, 90, 90)
-    context.font = "38px Arial";
-    context.fillStyle = "bisque";
-    context.fillText(value, x + 40, y + 65);
-  }
+setGame15();
 
-  const drawCell = function(position, value) {
-    if(position === 0) { drawBox(0, 0, value); }
-    else if (position === 1) { drawBox(100, 0, value); }
-    else if (position === 2) { drawBox(0, 100, value); }
-    else if (position === 3) { drawBox(100, 100, value); }
-  }
-
-  for (let i = 0; i<=8; i++) {
-    drawCell(i, arrayNums3[i])
-  }
-
-  const checkClientX = function(e) {
-    if (e < 100) { return 1; }
-    if (e < 200) { return 2; }
-  }
-
-  canvas.addEventListener("click", function(e) {
-    //console.log(e);
-    let check = checkClientX(e.offsetX);
-    if (e.offsetY < 100) {
-      switch(check) {
-        case 1: click = 0; break;
-        case 2: click = 1; break;
-      }
-    }
-    if (e.offsetY > 100 && e.offsetY < 200) {
-      switch(check) {
-        case 1: click = 2; break;
-        case 2: click = 3; break;
-      }
-    }
-    if (arrayNums3[click - 2] === 0) {
-      arrayNums3[click - 2] = arrayNums3[click];
-      arrayNums3[click] = 0;
-      counterMoves++;
-      counter_moves.textContent = counterMoves;
-    }
-    for (let i = 0; i<=3; i++) {
-      drawCell(i, arrayNums3[i])
-    }
-    if (arrayNums3[click + 2] === 0) {
-      arrayNums3[click + 2] = arrayNums3[click];
-      arrayNums3[click] = 0;
-      counterMoves++;
-      counter_moves.textContent = counterMoves;
-    }
-    for (let i = 0; i<=3; i++) {
-      drawCell(i, arrayNums3[i])
-    }
-    if (arrayNums3[click - 1] === 0 && click !== 2) {
-      arrayNums3[click - 1] = arrayNums3[click];
-      arrayNums3[click] = 0;
-      counterMoves++;
-      counter_moves.textContent = counterMoves;
-    }
-    for (let i = 0; i<=3; i++) {
-      drawCell(i, arrayNums3[i])
-    }
-    if (arrayNums3[click + 1] === 0 && click !== 1) {
-      arrayNums3[click + 1] = arrayNums3[click];
-      arrayNums3[click] = 0;
-      counterMoves++;
-      counter_moves.textContent = counterMoves;
-    }
-    for (let i = 0; i<=3; i++) {
-      drawCell(i, arrayNums3[i]);
-    }
-    //console.log(arrayNums3);
-    if (arrayNums3[0] === 1 && arrayNums3[1] === 2 && arrayNums3[2] === 3 && arrayNums3[3] === 0) {
-      victory_popup.classList.add('popup_active');
-      victory_popup.textContent = `Hooray! You solved the puzzle in ${mins.textContent}:${sec.textContent} and ${counter_moves.textContent} moves!`;
-      hidden_wrapper.classList.add('hidden_wrapper_active');
-    }
-  });
-}
-setGame3();
-
-hidden_wrapper.addEventListener('click', () => {
-  hidden_wrapper.classList.remove('hidden_wrapper_active');
-  victory_popup.classList.remove('popup_active');
-})
 function reset() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   counterMoves = 0;
@@ -606,14 +646,56 @@ function reset() {
   hours.textContent = '00';
   sec.textContent = '00';
   mins.textContent = '00';
+  arrayNums3 = [];
   arrayNums8 = [];
   arrayNums15 = [];
 }
+btn_22.addEventListener('click', () => {
+  reset();
+  setGame3();
+  change_size.textContent = '2x2';
+});
 btn_33.addEventListener('click', () => {
   reset();
   setGame8();
-})
+  change_size.textContent = '3x3';
+});
 btn_44.addEventListener('click', () => {
   reset();
   setGame15();
+  change_size.textContent = '4x4';
+});
+btn_55.addEventListener('click', () => {
+  change_size.textContent = '5x5';
+});
+btn_66.addEventListener('click', () => {
+  change_size.textContent = '6x6';
+});
+btn_77.addEventListener('click', () => {
+  change_size.textContent = '7x7';
+});
+btn_88.addEventListener('click', () => {
+  change_size.textContent = '8x8';
+});
+
+function restartGame() {
+  if(btn_22.classList.contains('btn_active')) {
+    reset();
+    setGame3();
+  }
+  if(btn_33.classList.contains('btn_active')) {
+    reset();
+    setGame8();
+  }
+  if(btn_44.classList.contains('btn_active')) {
+    reset();
+    setGame15();
+  }
+}
+restart_btn.addEventListener('click', restartGame);
+
+hidden_wrapper.addEventListener('click', () => {
+  hidden_wrapper.classList.remove('hidden_wrapper_active');
+  victory_popup.classList.remove('popup_active');
+  restartGame();
 })
