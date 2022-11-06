3d4ceeff60f178a601175302db98a0c2f2564144
Nastasyma <stacy.shishmareva@gmail.com>
2022-11-06 23:09:37 +0300

    feat: add video background, welcome page

---
+++ songbird/src/index.js
@@ -0,0 +1,7 @@
+import './assets/styles/style.css';
+import './assets/styles/normalize.css';
+import helper from './js/helper.js';
+import avg from './js/some.js';
+
+console.log(avg(1, 4, 8));
+console.log(helper(1,2));
