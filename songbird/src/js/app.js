dcd97c6e7168b144cc85bbf763a59268015bc1c2
Nastasyma <stacy.shishmareva@gmail.com>
2022-11-07 14:42:24 +0300

    feat: add game wrapper, add change from welcome page to game page

---
+++ songbird/src/js/app.js
@@ -0,0 +1,37 @@
+const videoBG = document.querySelector('.video_bg');
+const startBTN = document.querySelector('.game__start_btn');
+const welcomeBTN = document.querySelector('.welcome_link');
+const gameBTN = document.querySelector('.game_link');
+const gameStartWrapper = document.querySelector('.game__start_wrapper');
+const gameWrapper = document.querySelector('.game__wrapper');
+const videoFile = document.querySelector('.forest_video');
+
+function changeElStyles() {
+  videoFile.classList.add('active_hide');
+  gameStartWrapper.classList.add('active_hide');
+  setTimeout(() => {
+    gameWrapper.classList.add('active_game__menu');
+  }, 500);
+  gameBTN.classList.add('nav_link__active');
+  welcomeBTN.classList.remove('nav_link__active');
+  videoBG.style.background = "url('../../assets/images/bird_bg.jpg') center/cover no-repeat";
+}
+function startGame() {
+  startBTN.addEventListener('click', changeElStyles);
+  gameBTN.addEventListener('click', changeElStyles);
+}
+function returnToStart() {
+  welcomeBTN.addEventListener('click', () => {
+    videoFile.classList.remove('active_hide');
+    setTimeout(() => {
+      gameStartWrapper.classList.remove('active_hide');
+    }, 500);
+    // gameStartWrapper.classList.remove('active_hide');
+    gameBTN.classList.remove('nav_link__active');
+    welcomeBTN.classList.add('nav_link__active');
+    gameWrapper.classList.remove('active_game__menu');
+  })
+}
+
+export { startGame, returnToStart }
+
