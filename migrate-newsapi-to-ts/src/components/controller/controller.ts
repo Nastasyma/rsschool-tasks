fea33c2bd1e91590d11be005f462a797728ff0d4
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-06-07 23:51:32 +0300

    feat: migrate appLoader.js and controller.js to TS

--- migrate-newsapi-to-ts/src/components/controller/controller.ts
+++ migrate-newsapi-to-ts/src/components/controller/controller.ts
@@ -1,27 +1,30 @@
+import { IArticle } from '../../interfaces/article';
+import { ISource } from '../../interfaces/source';
+import ApiEndPoints from '../../types/endpoints';
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-  getSources(callback) {
+  public getSources(callback: (data?: ISource) => void): void {
     super.getResp(
       {
-        endpoint: 'sources',
+        endpoint: ApiEndPoints.sources,
       },
       callback,
     );
   }
 
-  getNews(e, callback) {
+  getNews(e: MouseEvent, callback: (data?: IArticle) => void) {
     let { target } = e;
     const newsContainer = e.currentTarget;
 
-    while (target !== newsContainer) {
+    while (target instanceof HTMLElement && newsContainer instanceof HTMLElement && target !== newsContainer) {
       if (target.classList.contains('source__item')) {
         const sourceId = target.getAttribute('data-source-id');
-        if (newsContainer.getAttribute('data-source') !== sourceId) {
+        if (sourceId && newsContainer.getAttribute('data-source') !== sourceId) {
           newsContainer.setAttribute('data-source', sourceId);
           super.getResp(
             {
-              endpoint: 'everything',
+              endpoint: ApiEndPoints.everything,
               options: {
                 sources: sourceId,
               },
