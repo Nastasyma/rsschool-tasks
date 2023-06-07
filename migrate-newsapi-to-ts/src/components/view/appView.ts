8766ba6faa0ee23ced1536f4b579db0e9d469333
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-06-07 15:32:09 +0300

    feat: migrate appView.js to TS

---
+++ migrate-newsapi-to-ts/src/components/view/appView.ts
@@ -0,0 +1,28 @@
+import News from './news/news';
+import Sources from './sources/sources';
+import { IData } from '../../interfaces/data';
+import { IArticle } from '../../interfaces/article';
+import { ISource } from '../../interfaces/source';
+
+export class AppView {
+  private news: News;
+
+  private sources: Sources;
+
+  constructor() {
+    this.news = new News();
+    this.sources = new Sources();
+  }
+
+  drawNews(data: IData) {
+    const values: IArticle[] = data?.articles ? data?.articles : [];
+    this.news.draw(values);
+  }
+
+  drawSources(data: IData) {
+    const values: ISource[] = data?.sources ? data?.sources : [];
+    this.sources.draw(values);
+  }
+}
+
+export default AppView;
--- migrate-newsapi-to-ts/src/interfaces/data.ts
+++ migrate-newsapi-to-ts/src/interfaces/data.ts
@@ -4,4 +4,8 @@ import { ISource } from './source';
 export interface IData {
   articles?: IArticle[];
   sources?: ISource[];
+  totalResults?: number;
+  status: string;
+  drawNews(data: IData): void;
+  drawSources(data: IData): void;
 }
