b642080b115470efa95a1afd46e9af2deb95baa8
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-06-08 23:00:47 +0300

    refactor: edit app styles and favicon

--- migrate-newsapi-to-ts/webpack.config.js
+++ migrate-newsapi-to-ts/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
   entry: path.resolve(__dirname, './src/index'),
@@ -17,10 +18,17 @@ const baseConfig = {
         use: 'ts-loader',
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        test: /\.(?:ico|gif|svg|png|jpg|jpeg)$/i,
         type: 'asset/resource',
         generator: {
           filename: 'img/[name][ext]',
+        },
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
         },
       },
     ],
@@ -32,13 +40,23 @@ const baseConfig = {
     filename: 'index.js',
     path: path.resolve(__dirname, './dist'),
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      favicon: 'src/img/favicon.ico',
+      favicon: 'src/favicon.ico',
       template: path.resolve(__dirname, './src/index.html'),
       filename: 'index.html',
     }),
     new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/img', to: 'img' },
+      ]
+    })
   ],
 };
 
