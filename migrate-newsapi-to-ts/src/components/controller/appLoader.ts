c6b596c71e0bf71eae79e318b4a327e1f6e6d61c
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-06-09 23:53:13 +0300

    feat: add app logo

--- migrate-newsapi-to-ts/src/components/controller/appLoader.ts
+++ migrate-newsapi-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,9 @@ import Loader from './loader';
 class AppLoader extends Loader {
   constructor() {
     super('https://news-proxy.spanb4.shop/', {
+      // 'https://newsapi.org/v2/'
       apiKey: 'ef417a2988654055bd3fa7c2a7ccb433',
+      // de951c35b8044fc4967ef959d760665a
     });
   }
 }
