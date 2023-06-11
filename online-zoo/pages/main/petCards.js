060393427bd5ec9d39aaa749277c9c613be138bc
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-06-12 00:30:11 +0300

    refactor: edit zoo project

---
+++ online-zoo/pages/main/petCards.js
@@ -0,0 +1,112 @@
+const animals = [
+  {
+    title: 'giant Pandas',
+    location: 'Native to Southwest China',
+    src: '../../assets/images/pandas_card.png',
+    meal: '../../assets/images/banana-bamboo_icon.png'
+  },
+  {
+    title: 'Eagles',
+    location: 'Native to South America',
+    src: '../../assets/images/eagles_card.png',
+    meal: '../../assets/images/meet-fish_icon.png'
+  },
+  {
+    title: 'Gorillas',
+    location: 'Native to Congo',
+    src: '../../assets/images/gorillas_card.png',
+    meal: '../../assets/images/banana-bamboo_icon.png'
+  },
+  {
+    title: 'Two-toed Sloth',
+    location: 'Mesoamerica, South America',
+    src: '../../assets/images/two_toed_sloth_card.png',
+    meal: '../../assets/images/banana-bamboo_icon.png'
+  },
+  {
+    title: 'Cheetahs',
+    location: 'Native to Africa',
+    src: '../../assets/images/cheetahs_card.png',
+    meal: '../../assets/images/meet-fish_icon.png'
+  },
+  {
+    title: 'Alligators',
+    location: 'Native to Southeastern U. S.',
+    src: '../../assets/images/alligator_card.png',
+    meal: '../../assets/images/meet-fish_icon.png'
+  },
+  {
+    title: 'Penguins',
+    location: 'Native to Antarctica',
+    src: '../../assets/images/penguins_card.png',
+    meal: '../../assets/images/meet-fish_icon.png'
+  },
+  {
+    title: 'Bear',
+    location: 'Native to Russia, Western Europe',
+    src: '../../assets/images/bear_card.png',
+    meal: '../../assets/images/meet-fish_icon.png'
+  },
+  {
+    title: 'Camel',
+    location: 'Native to North Africa',
+    src: '../../assets/images/camel_card.png',
+    meal: '../../assets/images/banana-bamboo_icon.png'
+  },
+  {
+    title: 'Elephant',
+    location: 'Native to Africa, Asia',
+    src: '../../assets/images/elephant_card.png',
+    meal: '../../assets/images/banana-bamboo_icon.png'
+  },
+  {
+    title: 'Giraffe',
+    location: 'Native to Africa',
+    src: '../../assets/images/giraffe_card.png',
+    meal: '../../assets/images/banana-bamboo_icon.png'
+  },
+  {
+    title: 'Gorillas',
+    location: 'Native to Congo',
+    src: '../../assets/images/gorillas2_card.png',
+    meal: '../../assets/images/banana-bamboo_icon.png'
+  },
+  {
+    title: 'Hippopotamus',
+    location: 'Native to Africa',
+    src: '../../assets/images/hippo_card.png',
+    meal: '../../assets/images/banana-bamboo_icon.png'
+  },
+  {
+    title: 'Kangaroo',
+    location: 'Native to Australia',
+    src: '../../assets/images/kangaroo_card.png',
+    meal: '../../assets/images/banana-bamboo_icon.png'
+  },
+  {
+    title: 'Lion',
+    location: 'Native to Africa',
+    src: '../../assets/images/lion_card.png',
+    meal: '../../assets/images/meet-fish_icon.png'
+  },
+  {
+    title: 'Tiger',
+    location: 'Native to Asia',
+    src: '../../assets/images/tiger_card.png',
+    meal: '../../assets/images/meet-fish_icon.png'
+  },
+  {
+    title: 'White bear',
+    location: 'Native to Arctic',
+    src: '../../assets/images/white_bear_card.png',
+    meal: '../../assets/images/meet-fish_icon.png'
+  },
+  {
+    title: 'Zebra',
+    location: 'Native to South, East Africa',
+    src: '../../assets/images/zebra_card.png',
+    meal: '../../assets/images/banana-bamboo_icon.png'
+  }
+  ]
+
+    export default animals;
