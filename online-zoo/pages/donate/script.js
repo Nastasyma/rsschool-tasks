77dfc9e59e58b233f57551b5aa2c4389d5b4e293
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-06-12 00:28:40 +0300

    refactor: edit zoo project

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -2,9 +2,14 @@ const email = document.querySelector('.email');
 const submitBTN = document.querySelector('.submit_btn');
 const emailForm = document.querySelector('.enter_email');
 const amountInput = document.querySelector('.another_amount_input');
+const dollarsGray = document.querySelector('.dollars_gray');
 const payNumber = document.querySelectorAll('.pay_number');
-const amount = document.getElementById('donate_input_value');
+const inputDonate1 = document.querySelector('.input1');
+const inputDonate2 = document.querySelector('.input2');
+const inputDonate3 = document.querySelector('.input3');
 document.documentElement.style.setProperty('--visibility', "hidden");
+document.documentElement.style.setProperty('--background', "url('../../assets/images/roller_icon.png')");
+document.documentElement.style.setProperty('--background2', "url('../../assets/images/roller_icon_small.png')");
 
 function correctForm(event) {
 	event.preventDefault();
@@ -14,7 +19,6 @@ function correctForm(event) {
   submitBTN.style.border = '1px solid black';
 }
 emailForm.addEventListener('submit', correctForm);
-
 function validate() {
   let pattern = /\S+@\S+/;
 
@@ -34,38 +38,226 @@ function validate() {
     submitBTN.style.border = '1px solid black';
   }
 }
-
 amountInput.oninput = function () {
   if (this.value.length > 4 || this.value < 0) {
     this.value = this.value.slice(0,4);
     document.documentElement.style.setProperty('--visibility', "visible");
     amountInput.style.border = '1px solid red';
     amountInput.style.outline = '1px solid red';
+    dollarsGray.style.color = 'red';
+    amountInput.style.color = 'red';
   } else {
     document.documentElement.style.setProperty('--visibility', "hidden");
     amountInput.style.border = '#929699';
     amountInput.style.outline = '1px solid black';
+    dollarsGray.style.color = '#929699';
+    amountInput.style.color = '#929699';
   }
 }
-
 amountInput.addEventListener('keydown', function(e) {
   if (e.keyCode === 13) {
     amountInput.value = "";
     document.documentElement.style.setProperty('--visibility', "hidden");
     amountInput.style.border = '#929699';
     amountInput.style.outline = '1px solid black';
+    for (let i=0; i<payNumber.length; i++) {
+      payNumber[i].classList.remove('active_number');
+    }
+    payNumber[5].classList.add('active_number');
+    inputDonate1.value = '5';
+    inputDonate2.value = '4';
+    inputDonate3.value = '2';
   }
 })
-
-/* payNumber.forEach(el => {
+payNumber.forEach(el => {
   el.addEventListener('click', (e) => {
+    setThumb();
     for (let i=0; i<payNumber.length; i++) {
       payNumber[i].classList.remove('active_number');
     }
     e.target.classList.add('active_number');
-  })
-})
+    //console.log(e.target.value)
+    inputDonate1.value = e.target.value;
+    inputDonate2.value = e.target.value-1;
+    inputDonate3.value = e.target.value-3;
+    if(e.target.value === '0') {
+      amountInput.value = '5000'
+    } else if(e.target.value === '1') {
+      amountInput.value = '2000'
+    } else if(e.target.value === '2') {
+      amountInput.value = '1000'
+    } else if(e.target.value === '3') {
+      amountInput.value = '500'
+    } else if(e.target.value === '4') {
+      amountInput.value = '250'
+    } else if(e.target.value === '5') {
+      amountInput.value = '100'
+    } else if(e.target.value === '6') {
+      amountInput.value = '50'
+    } else if(e.target.value === '7') {
+      amountInput.value = '25'
+    }
+  });
+});
+inputDonate1.addEventListener("change", function() {
+  setThumb();
+  //console.log(inputDonate1.value);
+  for (let i = 0; i < payNumber.length; i++) {
+      payNumber[i].classList.remove('active_number');
+      payNumber[inputDonate1.value].classList.add('active_number');
+      if(payNumber[0].classList.contains('active_number')) {
+        amountInput.value = '5000'
+      }
+      if(payNumber[1].classList.contains('active_number')) {
+        amountInput.value = '2000'
+      }
+      if(payNumber[2].classList.contains('active_number')) {
+        amountInput.value = '1000'
+      }
+      if(payNumber[3].classList.contains('active_number')) {
+        amountInput.value = '500'
+      }
+      if(payNumber[4].classList.contains('active_number')) {
+        amountInput.value = '250'
+      }
+      if(payNumber[5].classList.contains('active_number')) {
+        amountInput.value = '100'
+      }
+      if(payNumber[6].classList.contains('active_number')) {
+        amountInput.value = '50'
+      }
+      if(payNumber[7].classList.contains('active_number')) {
+        amountInput.value = '25'
+      }
+  }
+});
+inputDonate2.addEventListener("change", function() {
+  setThumb();
+  //console.log(inputDonate2.value);
+  for (let i = 0; i < payNumber.length; i++) {
+      payNumber[i].classList.remove('active_number');
+      payNumber[+inputDonate2.value+1].classList.add('active_number');
+      if(payNumber[1].classList.contains('active_number')) {
+        amountInput.value = '2000'
+      }
+      if(payNumber[2].classList.contains('active_number')) {
+        amountInput.value = '1000'
+      }
+      if(payNumber[3].classList.contains('active_number')) {
+        amountInput.value = '500'
+      }
+      if(payNumber[4].classList.contains('active_number')) {
+        amountInput.value = '250'
+      }
+      if(payNumber[5].classList.contains('active_number')) {
+        amountInput.value = '100'
+      }
+      if(payNumber[6].classList.contains('active_number')) {
+        amountInput.value = '50'
+      }
+      if(payNumber[7].classList.contains('active_number')) {
+        amountInput.value = '25'
+      }
+  }
+});
+inputDonate3.addEventListener("change", function() {
+  setThumb();
+  //console.log(inputDonate3.value);
+  for (let i = 0; i < payNumber.length; i++) {
+      payNumber[i].classList.remove('active_number');
+      payNumber[+inputDonate3.value+3].classList.add('active_number');
+      if(payNumber[3].classList.contains('active_number')) {
+        amountInput.value = '500'
+      }
+      if(payNumber[4].classList.contains('active_number')) {
+        amountInput.value = '250'
+      }
+      if(payNumber[5].classList.contains('active_number')) {
+        amountInput.value = '100'
+      }
+      if(payNumber[6].classList.contains('active_number')) {
+        amountInput.value = '50'
+      }
+      if(payNumber[7].classList.contains('active_number')) {
+        amountInput.value = '25'
+      }
+  }
+});
+function setThumb() {
+  document.documentElement.style.setProperty('--background', "url('../../assets/images/roller_icon.png')");
+  document.documentElement.style.setProperty('--background2', "url('../../assets/images/roller_icon_small.png')");
+}
+function changeAmount() {
+  amountInput.addEventListener("input", function() {
+    //console.log(amountInput.value);
+    for (let i=0; i<payNumber.length; i++) {
+      payNumber[i].classList.remove('active_number');
+    }
+    document.documentElement.style.setProperty('--background', "none");
+    document.documentElement.style.setProperty('--background2', "none");
+    if(amountInput.value === '5000') {
+      payNumber[0].classList.add('active_number');
+      inputDonate1.value = '0';
+      setThumb();
+    }
+    if(amountInput.value === '2000') {
+      payNumber[1].classList.add('active_number');
+      inputDonate1.value = '1';
+      inputDonate2.value = '0';
+      setThumb();
+    }
+    if(amountInput.value === '1000') {
+      payNumber[2].classList.add('active_number');
+      inputDonate1.value = '2';
+      inputDonate2.value = '1';
+      setThumb();
+    }
+    if(amountInput.value === '500') {
+      payNumber[3].classList.add('active_number');
+      inputDonate1.value = '3';
+      inputDonate2.value = '2';
+      inputDonate3.value = '0';
+      setThumb();
+    }
+    if(amountInput.value === '250') {
+      payNumber[4].classList.add('active_number');
+      inputDonate1.value = '4';
+      inputDonate2.value = '3';
+      inputDonate3.value = '1';
+      setThumb();
+    }
+    if(amountInput.value === '100') {
+      payNumber[5].classList.add('active_number');
+      inputDonate1.value = '5';
+      inputDonate2.value = '4';
+      inputDonate3.value = '2';
+      setThumb();
+    }
+    if(amountInput.value === '50') {
+      payNumber[6].classList.add('active_number');
+      inputDonate1.value = '6';
+      inputDonate2.value = '5';
+      inputDonate3.value = '3';
+      setThumb();
+    }
+    if(amountInput.value === '25') {
+      payNumber[7].classList.add('active_number');
+      inputDonate1.value = '7';
+      inputDonate2.value = '6';
+      inputDonate3.value = '4';
+      setThumb();
+    }
+    if(amountInput.value === '5000' && window.innerWidth <= 1000) {
+      document.documentElement.style.setProperty('--background', "none");
+    }
+    if((amountInput.value === '2000' || amountInput.value === '1000') && window.innerWidth <= 640) {
+      document.documentElement.style.setProperty('--background', "none");
+      document.documentElement.style.setProperty('--background2', "none");
+    }
+  });
+}
+changeAmount();
 
-amount.addEventListener("change", function() {
-  console.log(this.value);
-}); */
+window.addEventListener('resize', () => {
+  changeAmount();
+});
--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,6 +1,21 @@
+import animals from './petCards.js';
+
 const email = document.querySelector('.email');
 const submitBTN = document.querySelector('.submit_btn');
 const emailForm = document.querySelector('.enter_email');
+const petsPhotoWrapper = document.querySelector('.pets_photo_wrapper');
+const BTNleft = document.querySelector('.left_btn');
+const BTNright = document.querySelector('.right_btn');
+const section1 = document.querySelector('.section1');
+const section2 = document.querySelector('.section2');
+const section3 = document.querySelector('.section3');
+const scrollBar = document.getElementById('scrollBar');
+const userCard = document.querySelector('.user_card');
+const userCards = document.querySelectorAll('.user_card');
+const testimonialsPopupWrapper = document.querySelector('.testimonials_popup_wrapper');
+const testimonialsPopup = document.querySelector('.testimonials_popup');
+const mobileWrapperPopup = document.querySelector('.mobile_wrapper_popup');
+const closeIcon = document.querySelector('.close_icon');
 
 function correctForm(event) {
 	event.preventDefault();
@@ -30,3 +45,410 @@ function validate() {
     submitBTN.style.border = '1px solid black';
   }
 }
+
+let animalsList1 = [];
+let animalsList2 = [];
+let animalsList3 = [];
+let petIndex;
+
+function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function sortAnimals() {
+  animalsList1.length = 0;
+  animalsList2.length = 0;
+  animalsList3.length = 0;
+  if (window.innerWidth > 640) {
+    for (let i=0; i<6; i++) {
+      petIndex = getRandomNum(0, animals.length-1);
+      //console.log("petIndex =", petIndex);
+      if (animalsList1.includes(animals[petIndex])) {
+        i--
+      } else {
+        animalsList1.push(animals[petIndex]);
+      }
+    }
+    for (let i=0; i<6; i++) {
+      petIndex = getRandomNum(0, animals.length-1);
+      //console.log("petIndex =", petIndex);
+      if (animalsList2.includes(animals[petIndex])) {
+        i--
+      } else {
+        animalsList2.push(animals[petIndex]);
+      }
+    }
+    for (let i=0; i<6; i++) {
+      petIndex = getRandomNum(0, animals.length-1);
+      //console.log("petIndex =", petIndex);
+      if (animalsList3.includes(animals[petIndex])) {
+        i--
+      } else {
+        animalsList3.push(animals[petIndex]);
+      }
+    }
+  }
+  if (window.innerWidth <= 640) {
+    for (let i=0; i<4; i++) {
+      petIndex = getRandomNum(0, animals.length-1);
+      //console.log("petIndex =", petIndex);
+      if (animalsList1.includes(animals[petIndex])) {
+        i--
+      } else {
+        animalsList1.push(animals[petIndex]);
+      }
+    }
+    for (let i=0; i<4; i++) {
+      petIndex = getRandomNum(0, animals.length-1);
+      //console.log("petIndex =", petIndex);
+      if (animalsList2.includes(animals[petIndex])) {
+        i--
+      } else {
+        animalsList2.push(animals[petIndex]);
+      }
+    }
+    for (let i=0; i<4; i++) {
+      petIndex = getRandomNum(0, animals.length-1);
+      //console.log("petIndex =", petIndex);
+      if (animalsList3.includes(animals[petIndex])) {
+        i--
+      } else {
+        animalsList3.push(animals[petIndex]);
+      }
+    }
+  }
+  //console.log("animalsList1 =", animalsList1);
+  //console.log("animalsList2 =", animalsList2);
+  //console.log("animalsList3 =", animalsList3);
+}
+sortAnimals();
+
+function createPetCard1() {
+  for (let i = 0; i < animalsList1.length; i++) {
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('pet_card');
+    const div1 = document.createElement('div');
+    div1.classList.add('card_photo_discription');
+    const div2 = document.createElement('div');
+    div2.classList.add('pet_description');
+    mainDiv.appendChild(div1);
+    mainDiv.appendChild(div2);
+    let image1 = new Image();
+    image1.classList.add('card');
+    image1.src = animalsList1[i].src;
+    image1.alt = "animal_photo"
+    div1.appendChild(image1);
+    const div3 = document.createElement('div');
+    div3.classList.add('pet_description_text');
+    div2.appendChild(div3);
+    const span1 = document.createElement('span');
+    span1.classList.add('pet_title');
+    span1.textContent = animalsList1[i].title;
+    const span2 = document.createElement('span');
+    span2.classList.add('pet_location');
+    span2.textContent = animalsList1[i].location;
+    div3.appendChild(span1);
+    div3.appendChild(span2);
+    let image2 = new Image();
+    if (animalsList1[i].meal === '../../assets/images/banana-bamboo_icon.png') {
+      image2.classList.add('banana_icon');
+      image2.alt = "banana_icon"
+    }
+    if (animalsList1[i].meal === '../../assets/images/meet-fish_icon.png') {
+      image2.classList.add('meet-fish_icon');
+      image2.alt = "meet-fish_icon"
+    }
+    image2.src = animalsList1[i].meal;
+    div2.appendChild(image2);
+    section1.append(mainDiv);
+  }
+}
+function createPetCard2() {
+  for (let i = 0; i < animalsList2.length; i++) {
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('pet_card');
+    const div1 = document.createElement('div');
+    div1.classList.add('card_photo_discription');
+    const div2 = document.createElement('div');
+    div2.classList.add('pet_description');
+    mainDiv.appendChild(div1);
+    mainDiv.appendChild(div2);
+    let image1 = new Image();
+    image1.classList.add('card');
+    image1.src = animalsList2[i].src;
+    image1.alt = "animal_photo"
+    div1.appendChild(image1);
+    const div3 = document.createElement('div');
+    div3.classList.add('pet_description_text');
+    div2.appendChild(div3);
+    const span1 = document.createElement('span');
+    span1.classList.add('pet_title');
+    span1.textContent = animalsList2[i].title;
+    const span2 = document.createElement('span');
+    span2.classList.add('pet_location');
+    span2.textContent = animalsList2[i].location;
+    div3.appendChild(span1);
+    div3.appendChild(span2);
+    let image2 = new Image();
+    if (animalsList2[i].meal === '../../assets/images/banana-bamboo_icon.png') {
+      image2.classList.add('banana_icon');
+      image2.alt = "banana_icon"
+    }
+    if (animalsList2[i].meal === '../../assets/images/meet-fish_icon.png') {
+      image2.classList.add('meet-fish_icon');
+      image2.alt = "meet-fish_icon"
+    }
+    image2.src = animalsList2[i].meal;
+    div2.appendChild(image2);
+    section2.append(mainDiv);
+  }
+}
+function createPetCard3() {
+  for (let i = 0; i < animalsList3.length; i++) {
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('pet_card');
+    const div1 = document.createElement('div');
+    div1.classList.add('card_photo_discription');
+    const div2 = document.createElement('div');
+    div2.classList.add('pet_description');
+    mainDiv.appendChild(div1);
+    mainDiv.appendChild(div2);
+    let image1 = new Image();
+    image1.classList.add('card');
+    image1.src = animalsList3[i].src;
+    image1.alt = "animal_photo"
+    div1.appendChild(image1);
+    const div3 = document.createElement('div');
+    div3.classList.add('pet_description_text');
+    div2.appendChild(div3);
+    const span1 = document.createElement('span');
+    span1.classList.add('pet_title');
+    span1.textContent = animalsList3[i].title;
+    const span2 = document.createElement('span');
+    span2.classList.add('pet_location');
+    span2.textContent = animalsList3[i].location;
+    div3.appendChild(span1);
+    div3.appendChild(span2);
+    let image2 = new Image();
+    if (animalsList3[i].meal === '../../assets/images/banana-bamboo_icon.png') {
+      image2.classList.add('banana_icon');
+      image2.alt = "banana_icon"
+    }
+    if (animalsList3[i].meal === '../../assets/images/meet-fish_icon.png') {
+      image2.classList.add('meet-fish_icon');
+      image2.alt = "meet-fish_icon"
+    }
+    image2.src = animalsList3[i].meal;
+    div2.appendChild(image2);
+    section3.append(mainDiv);
+  }
+}
+createPetCard1();
+createPetCard2();
+createPetCard3();
+
+petsPhotoWrapper.addEventListener('animationend', (animationEvent) => {
+
+  let changedItem;
+  if (animationEvent.animationName === 'move-left') {
+    petsPhotoWrapper.classList.remove('to-left');
+    changedItem = section1;
+    section2.innerHTML = changedItem.innerHTML;
+    changedItem.innerHTML = "";
+    sortAnimals();
+    createPetCard1();
+  } else if (animationEvent.animationName === 'move-right') {
+    petsPhotoWrapper.classList.remove('to-right');
+    changedItem = section3;
+    section2.innerHTML = changedItem.innerHTML;
+    changedItem.innerHTML = "";
+    sortAnimals();
+    createPetCard3();
+  }
+
+  BTNleft.addEventListener('click', moveLeft);
+  BTNright.addEventListener('click', moveRight);
+});
+
+const moveLeft = () => {
+  petsPhotoWrapper.classList.add('to-left');
+  BTNleft.removeEventListener('click', moveLeft);
+  BTNright.removeEventListener('click', moveRight);
+};
+const moveRight = () => {
+  petsPhotoWrapper.classList.add('to-right');
+  BTNright.removeEventListener('click', moveRight);
+  BTNleft.removeEventListener('click', moveLeft);
+};
+
+BTNleft.addEventListener('click', moveLeft);
+BTNright.addEventListener('click', moveRight);
+
+function reset() {
+  section1.innerHTML = '';
+  section2.innerHTML = '';
+  section3.innerHTML = '';
+}
+function changeReviewValue() {
+  if (window.innerWidth <= 830) {
+    userCard.style.marginLeft = '0';
+    scrollBar.value = '0'
+    //console.log(scrollBar.value);
+  }
+  if (window.innerWidth > 1000) {
+    if (scrollBar.value === '0') {
+      userCard.style.marginLeft = '0';
+    }
+    if (scrollBar.value === '1') {
+      userCard.style.marginLeft = '-25.5%';
+    }
+    if (scrollBar.value === '2') {
+      userCard.style.marginLeft = '-51%';
+    }
+    if (scrollBar.value === '3') {
+      userCard.style.marginLeft = '-76.5%';
+    }
+    if (scrollBar.value === '4') {
+      userCard.style.marginLeft = '-102%';
+    }
+    if (scrollBar.value === '5') {
+      userCard.style.marginLeft = '-127.5%';
+    }
+    if (scrollBar.value === '6') {
+      userCard.style.marginLeft = '-153%';
+    }
+    if (scrollBar.value === '7') {
+      userCard.style.marginLeft = '-178.5%';
+    }
+  }
+  if (window.innerWidth <= 1000) {
+    if (scrollBar.value === '0') {
+      userCard.style.marginLeft = '0';
+    }
+    if (scrollBar.value === '1') {
+      userCard.style.marginLeft = '-34%';
+    }
+    if (scrollBar.value === '2') {
+      userCard.style.marginLeft = '-68%';
+    }
+    if (scrollBar.value === '3') {
+      userCard.style.marginLeft = '-102%';
+    }
+    if (scrollBar.value === '4') {
+      userCard.style.marginLeft = '-136%';
+    }
+    if (scrollBar.value === '5') {
+      userCard.style.marginLeft = '-171%';
+    }
+    if (scrollBar.value === '6') {
+      userCard.style.marginLeft = '-205%';
+    }
+    if (scrollBar.value === '7') {
+      userCard.style.marginLeft = '-239%';
+    }
+  }
+  scrollBar.addEventListener("input", () => {
+    //console.log(scrollBar.value);
+    if (window.innerWidth > 1000) {
+      if (scrollBar.value === '0') {
+        userCard.style.marginLeft = '0';
+      }
+      if (scrollBar.value === '1') {
+        userCard.style.marginLeft = '-25.5%';
+      }
+      if (scrollBar.value === '2') {
+        userCard.style.marginLeft = '-51%';
+      }
+      if (scrollBar.value === '3') {
+        userCard.style.marginLeft = '-76.5%';
+      }
+      if (scrollBar.value === '4') {
+        userCard.style.marginLeft = '-102%';
+      }
+      if (scrollBar.value === '5') {
+        userCard.style.marginLeft = '-127.5%';
+      }
+      if (scrollBar.value === '6') {
+        userCard.style.marginLeft = '-153%';
+      }
+      if (scrollBar.value === '7') {
+        userCard.style.marginLeft = '-178.5%';
+      }
+    }
+    if (window.innerWidth <= 1000) {
+      if (scrollBar.value === '0') {
+        userCard.style.marginLeft = '0';
+      }
+      if (scrollBar.value === '1') {
+        userCard.style.marginLeft = '-34%';
+      }
+      if (scrollBar.value === '2') {
+        userCard.style.marginLeft = '-68%';
+      }
+      if (scrollBar.value === '3') {
+        userCard.style.marginLeft = '-102%';
+      }
+      if (scrollBar.value === '4') {
+        userCard.style.marginLeft = '-136%';
+      }
+      if (scrollBar.value === '5') {
+        userCard.style.marginLeft = '-171%';
+      }
+      if (scrollBar.value === '6') {
+        userCard.style.marginLeft = '-205%';
+      }
+      if (scrollBar.value === '7') {
+        userCard.style.marginLeft = '-239%';
+      }
+    }
+  });
+}
+changeReviewValue();
+
+function popupReview() {
+  if (window.innerWidth <= 830) {
+    userCards.forEach(el => {
+      el.addEventListener('click', (e) => {
+        testimonialsPopupWrapper.classList.add('testimonials_popup_active');
+        mobileWrapperPopup.classList.add('popup_active');
+        mobileWrapperPopup.addEventListener('click', () => {
+          testimonialsPopupWrapper.classList.remove('testimonials_popup_active');
+          mobileWrapperPopup.classList.remove('popup_active');
+        })
+        closeIcon.addEventListener('click', () => {
+          testimonialsPopupWrapper.classList.remove('testimonials_popup_active');
+          mobileWrapperPopup.classList.remove('popup_active');
+        })
+        const currentCard = e.target.closest(".user_card");
+        if(currentCard) {
+          testimonialsPopup.innerHTML = "";
+          testimonialsPopup.append(currentCard.cloneNode(true));
+          document.querySelector('.testimonials_popup > .user_card > .user_review').style.height = '309px';
+          document.querySelector('.testimonials_popup > .user_card').style.paddingRight = '10px';
+          document.querySelector('.testimonials_popup > .user_card').style.width = '100%';
+          document.querySelectorAll('.testimonials_popup > .user_card > .user_review > p').forEach(element => {
+            element.style.paddingRight = '20px';
+          });
+          document.querySelector('.testimonials_popup > .user_card > .user_review').style.overflow = 'auto';
+        }
+      });
+    });
+  }
+}
+popupReview();
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth === 640 || window.innerWidth === 1000 || window.innerWidth === 1600) {
+    reset();
+    sortAnimals()
+    createPetCard1();
+    createPetCard2();
+    createPetCard3();
+  }
+  changeReviewValue();
+  popupReview();
+}, true);
+
+
