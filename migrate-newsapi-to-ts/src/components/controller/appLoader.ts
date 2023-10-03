5f158c4bc6491e0862e4a1b267df0e0dd339e800
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-10-03 23:41:10 +0300

    refactor: edit proxy

--- migrate-newsapi-to-ts/src/components/controller/appLoader.ts
+++ migrate-newsapi-to-ts/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://news-proxy.spanb4.shop/', {
-      // 'https://newsapi.org/v2/'
+    super('https://rss-news-api.onrender.com/', {
+      // 'https://newsapi.org/v2/ https://rss-news-api.onrender.com/ https://news-proxy.spanb4.shop/'
       apiKey: 'ef417a2988654055bd3fa7c2a7ccb433',
       // de951c35b8044fc4967ef959d760665a
       // ef417a2988654055bd3fa7c2a7ccb433
